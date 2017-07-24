addc2b0d89e8f3a0dab9221bdce0faf217141159
Gigo <gigo@aufmbergschneits.net>
2017-07-24 21:39:16 +0200

    minimal kommentiert

--- Arduino/Lampe/Lampe.ino
+++ Arduino/Lampe/Lampe.ino
@@ -1,22 +1,21 @@
-// the setup routine runs once when you press reset:
+// Einstellungen werden bei Aktivierung von der setup()-Methode automatisch vorgenommen:
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
   lichtaus_init();
   ui_init();
 }
 
+//Attribute für die Farben Rot Grün Blau (im Regenbogenmodus)
 int regenbogen_r = 255;
 int regenbogen_g = 0;
 int regenbogen_b = 0;
 
+//Attribute für die Farben Rot Grün Blau (im Regenbogenmodus)
 int r = 0;
 int g = 0;
 int b = 0;
 
-
-
-
+//Methode, die die Farbe im Regenbogen einen winzigen Schritt weiter schaltet
 void regenbogen(void)
 {
   if(regenbogen_b == 0 && regenbogen_r > 0) {
@@ -32,26 +31,21 @@ void regenbogen(void)
   lichtaus_setLeds(regenbogen_r, regenbogen_g, regenbogen_b);
 }
 
-// the loop routine runs over and over again forever:
+// Die Loop-Methode läuft automatisch nach der Setup-Methode in einer Endlosschleife.
 void loop() {
-  // read the input on analog pin 0:
   int lichtLevel = lichtein_getLevel();
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = lichtLevel * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
 
-  //int outputValue = lichtLevel / 4;
-  //lichtaus_setLeds(outputValue, outputValue, outputValue);
-
-  if(!ui_isPressed(1) && !ui_isPressed(2) && !ui_isPressed(3) && !ui_isPressed(14) && r == 0 && g == 0 && b == 0 && lichtLevel > 300 ){
-    regenbogen();
-    ui_setLed(1, 0);
-    ui_setLed(2, 0);
-    ui_setLed(3, 0);
-    ui_setLed(4, 0);
+  if(
+      !ui_isPressed(1) && !ui_isPressed(2) && !ui_isPressed(3) && !ui_isPressed(14) 
+      && r == 0 && g == 0 && b == 0 
+      && lichtLevel > 300 
+    ){
+    ui_setLed(4, 1);
+    regenbogen(); 
     delay(10);
+    ui_setLed(4, 0);
   } else {
+    
     if(ui_isPressed(1)) {
       while (ui_isPressed(1)) {delay(10);}
       if (r==0) {
